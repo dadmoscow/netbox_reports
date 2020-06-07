d83a81f9814067bac1e8be173915b5bcf4ce08db
ryanmerolle <ryan.merolle@gmail.com>
2020-06-07 16:18:57 -0400

    seperated log messages for interface comparisons

--- reports/custom_reports.py
+++ reports/custom_reports.py
@@ -8,15 +8,18 @@ class DeviceAndTemplate(Report):
     description = "Check for differences in the template and corresponding devices"
 
     def test_count_interfaces(self):
+
         active = DeviceStatusChoices.STATUS_ACTIVE
         planned = DeviceStatusChoices.STATUS_PLANNED
 
         for device in Device.objects.filter(status__in=[active, planned]):
+
             # get device physical interface count
             device_interface_count = len([ i['id'] for i in device.interfaces.exclude(type__in=["virtual", "lag"]).values()])
 
             # get device template interface count
             device_template_interface_count = DeviceType.objects.filter(id = device.device_type_id)[0].interface_templates.exclude(type__in=["virtual", "lag"]).count()
+
             if device_interface_count != device_template_interface_count:
                 self.log_warning(
                         device,
@@ -28,9 +31,12 @@ class DeviceAndTemplate(Report):
                             )
 
     def test_interface_name(self):
+
         active = DeviceStatusChoices.STATUS_ACTIVE
         planned = DeviceStatusChoices.STATUS_PLANNED
+
         for device in Device.objects.filter(status__in=[active, planned]):
+
                       # get all physical device interfaces names
            device_interface_names = [inf['name'] for inf in device.interfaces.exclude(type__in=["virtual", "lag"]).values()]
 
@@ -39,18 +45,25 @@ class DeviceAndTemplate(Report):
 
            # get all device template interfaces names
            template_interface_names = [inf['name'] for inf in device_template.interface_templates.exclude(type__in=["virtual", "lag"]).values()]
+
            if device_interface_names == template_interface_names:
                self.log_success(
                        device
                    )
            else:
-               # get dict with count names interface from device + template
-               all_interfaces = Counter(device_interface_names + template_interface_names)
-               # get unique values
-               bad_interfaces = [name for name in all_interfaces.keys() if all_interfaces[name] == 1]
+               missing_device_ints = list(set(device_interface_names).difference(template_interface_names))
+               missing_devicetype_ints = list(set(template_interface_names).difference(device_interface_names))
+
+               if len(missing_device_ints) > 0:
+                   self.log_warning(
+                       device,
+                       "Interfaces on Device NOT on the DeviceType: " + str(missing_device_ints)
+                   )
+
+               if len(missing_devicetype_ints) > 0:
                self.log_warning(
                        device,
-                   "Missing interfaces: {}".format(bad_interfaces)
+                       "Interfaces on DeviceType NOT on the Device: " + str(missing_devicetype_ints)
                )
 
 
