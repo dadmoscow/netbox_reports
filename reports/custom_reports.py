4b8c9e021bab0118cacf54c86029f9d07782e120
ryanmerolle <ryan.merolle@gmail.com>
2020-06-07 13:21:55 -0400

    update report to work with NetBox 2.8

--- reports/custom_reports.py
+++ reports/custom_reports.py
@@ -1,4 +1,4 @@
-from dcim.constants import DEVICE_STATUS_ACTIVE, IFACE_FF_VIRTUAL, IFACE_FF_LAG
+from dcim.choices import DeviceStatusChoices
 from dcim.models import Device, DeviceType, Interface
 from extras.reports import Report
 from collections import Counter
@@ -8,45 +8,60 @@ class DeviceAndTemplate(Report):
     description = "Check for differences in the template and corresponding devices"
 
     def test_count_interfaces(self):
-        for device in Device.objects.filter(status = DEVICE_STATUS_ACTIVE):
-            # get device physical inteface count
-            device_interface_count = len([ i['id'] for i in device.interfaces.values() if i['form_factor'] not in (IFACE_FF_VIRTUAL, IFACE_FF_LAG) ])
-            
-            # get device template inteface count
-            device_template_interface_count = DeviceType.objects.filter(id = device.device_type_id)[0].interface_templates.count()
+        active = DeviceStatusChoices.STATUS_ACTIVE
+        planned = DeviceStatusChoices.STATUS_PLANNED
 
+        for device in Device.objects.filter(status__in=[active, planned]):
+            # get device physical interface count
+            device_interface_count = len([ i['id'] for i in device.interfaces.exclude(type__in=["virtual", "lag"]).values()])
+
+            # get device template interface count
+            device_template_interface_count = DeviceType.objects.filter(id = device.device_type_id)[0].interface_templates.exclude(type__in=["virtual", "lag"]).count()
             if device_interface_count != device_template_interface_count:
-                self.log_warning(device, "Interfaces (Template) = {:d}, Interfaces (Device) = {:d}".format(device_template_interface_count,device_interface_count)) 
+                self.log_warning(
+                        device,
+                        device.device_type.display_name + " has  " + str(device_template_interface_count) + ", " + device.name + " has " + str(device_interface_count)
+                        )
             else:
-                self.log_success(device)
+                    self.log_success(
+                            device
+                            )
+            def test_interface_name(self):
+                active = DeviceStatusChoices.STATUS_ACTIVE
+                planned = DeviceStatusChoices.STATUS_PLANNED
+                for device in Device.objects.filter(status__in=[active, planned]):
+                              # get all physical device interfaces names
+                   device_interface_names = [inf['name'] for inf in device.interfaces.exclude(type__in=["virtual", "lag"]).values()]
 
-    def test_interface_name(self):
-       for device in Device.objects.filter(status = DEVICE_STATUS_ACTIVE):
-           
-           # get all physical device interfaces names
-           device_interface_names = [inf['name'] for inf in device.interfaces.values() if inf['form_factor'] not in (IFACE_FF_VIRTUAL, IFACE_FF_LAG)]
-           # get device template
-           device_template = DeviceType.objects.filter(id = device.device_type_id)[0]
-           # get all device template interfaces names
-           template_interface_names = [inf['name'] for inf in device_template.interface_templates.values()]
+                   # get device template
+                   device_template = DeviceType.objects.filter(id = device.device_type_id)[0]
 
-           if device_interface_names == template_interface_names:
-               self.log_success(device)
-           else:
-               # get dict with count names interface from device + template
-               all_interfaces = Counter(device_interface_names + template_interface_names)
-               # get unique values
-               bad_interfaces = [name for name in all_interfaces.keys() if all_interfaces[name] == 1]
-               self.log_warning(device, "Missing interfaces: {}".format(bad_interfaces))
+                   # get all device template interfaces names
+                   template_interface_names = [inf['name'] for inf in device_template.interface_templates.exclude(type__in=["virtual", "lag"]).values()]
+                   if device_interface_names == template_interface_names:
+                       self.log_success(
+                               device
+                           )
+                   else:
+                       # get dict with count names interface from device + template
+                       all_interfaces = Counter(device_interface_names + template_interface_names)
+                       # get unique values
+                       bad_interfaces = [name for name in all_interfaces.keys() if all_interfaces[name] == 1]
+                       self.log_warning(
+                               device,
+                           "Missing interfaces: {}".format(bad_interfaces)
+                       )
 
 
 class InterfaceConnection(Report):
     description = "Check for lost physical interface connections"
     
     def test_interface_connection(self):
-        for device in Device.objects.filter(status = DEVICE_STATUS_ACTIVE):
+        active = DeviceStatusChoices.STATUS_ACTIVE
+        planned = DeviceStatusChoices.STATUS_PLANNED
+        for device in Device.objects.filter(status__in=[active, planned]):
             # get list with all physical device interfaces
-            device_interfaces = [ inf['id'] for inf in device.interfaces.values() if inf['form_factor'] not in (IFACE_FF_VIRTUAL, IFACE_FF_LAG) ]
+            device_interfaces = [ inf['id'] for inf in device.interfaces.exclude(type__in=["virtual", "lag"]).values() ]
             
             # if exist physical interfaces -> check it, else not exist -> success
             if device_interfaces:
@@ -61,9 +76,15 @@ class InterfaceConnection(Report):
                         
                 # if exist not connected interfaces -> warning, else -> success
                 if inf_not_connected:
-                    self.log_warning(device, "Not connected interfaces: {}".format(inf_not_connected))
+                    self.log_warning(
+                        device,
+                        "Not connected interfaces: {}".format(inf_not_connected)
+                        )
                 else:
-                    self.log_success(device)
+                    self.log_success(
+                        device
+                        )
             else:
-                self.log_success(device)
-                
+                self.log_success(
+                    device
+                    )
